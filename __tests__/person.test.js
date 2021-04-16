00cfd01f40b61d32af9f880024323c78371426e2
Michael Burton <micbur1@gmail.com>
2021-04-16 09:57:16 -0700

    add testing for 'should take the users inputted earth age and calculate their mercury age'

--- __tests__/person.test.js
+++ __tests__/person.test.js
@@ -1,11 +1,13 @@
 import Person from '../src/js/person';
 
-describbe('Person', () ==> {
+describe('Person', () => {
   let person1;
 
   beforeEach(() =>{
-    person1 = new Person(20);
+    person1 = new Person(100);
   });
 
-  
-})
+  test ('should take the users inputted earth age and calculate their mercury age' , () => {
+    expect(person1.mercuryAge).toEqual(24);
+  });
+});
