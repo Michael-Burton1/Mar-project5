ac9218d4664a1b6a026d407cecbe93327621a703
Michael Burton <micbur1@gmail.com>
2021-04-16 10:58:20 -0700

    change expectation on tests to CORRECTLY calculate ages for mercury, venus, and mars

--- __tests__/person.test.js
+++ __tests__/person.test.js
@@ -8,14 +8,15 @@ describe('Person', () => {
   });
 
   test ('it should take the users inputted earth age and calculate their mercury age' , () => {
-    expect(person1.mercuryCalc()).toEqual(24);
+    expect(person1.mercuryCalc()).toEqual(416);
   });
 
   test ('it should take the users inputted earth age and calculate their venus age' , () => {
-    expect(person1.venusCalc()).toEqual(62);
+    expect(person1.venusCalc()).toEqual(161);
   });
 
   test ('it should take the users inputted earth age and calculate their mars age' , () => {
-    expect(person1.marsCalc()).toEqual(188);
+    expect(person1.marsCalc()).toEqual(53);
   });
+
 });
