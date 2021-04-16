72979d69be69d134b5fae2117cfb641c9a84ef22
Michael Burton <micbur1@gmail.com>
2021-04-16 09:46:47 -0700

    rename files and folders in tests folder and js folder from template names to relivant project names and start makeing the person class in person.js

---
+++ __tests__/person.test.js
@@ -0,0 +1,11 @@
+import Person from '../src/js/person';
+
+describbe('Person', () ==> {
+  let person1;
+
+  beforeEach(() =>{
+    person1 = new Person(20);
+  });
+
+  
+})
---
+++ src/js/person.js
@@ -0,0 +1,25 @@
+export default class Person {
+
+  constructor (earthAge, mercuryAge, venusAge, marsAge, jupiterAge) {
+    this.earthAge = earthAge;
+    this.mercuryAge = mercuryAge;
+    this.venusAge = venusAge;
+    this.marsAge = marsAge;
+    this.jupiterAge = jupiterAge;
+  }
+
+  mercuryCalc() {
+
+  }
+
+  venusCalc() {
+
+  }
+
+  marsCalc() {
+
+  }
+  JupiterCalc() {
+    
+  }
+}
